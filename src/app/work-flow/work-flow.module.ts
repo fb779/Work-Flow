677f132a184d0f3e9473c6d13335ac2d765072b2
Fabian Forero <fabianforero779@gmail.com>
2021-12-12 19:44:35 -0500

    Add ChartModule to WorkFlowModule

--- src/app/work-flow/work-flow.module.ts
+++ src/app/work-flow/work-flow.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { ChartModule } from 'angular2-chartjs';
 import { WorkFlowService } from './work-flow.service';
 
 import { GanttComponent } from './components/gantt/gantt.component';
@@ -18,7 +19,7 @@ import { AverageTimeTableComponent } from './components/average-time-table/avera
     WorkFlowComponent,
     AverageTimeTableComponent,
   ],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ChartModule],
   exports: [WorkFlowComponent],
   providers: [WorkFlowService],
 })
